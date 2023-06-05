214b468cbfd27a41db855f0ea356daae420bb70e
Samuel Abu <abu.sammie@gmail.com>
2023-06-05 16:03:19 +0200

    books h3 mb added

--- components/Books.js
+++ components/Books.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 function Books() {
   return (
     <main className='bg-sky-50 py-10'>
-        <h3 className='text-center text-3xl font-bold underline decoration-4 underline-offset-8'>Books</h3>
+        <h3 className='text-center text-3xl font-bold underline decoration-4 underline-offset-8 mb-5'>Books</h3>
         <div className='py-9 grid grid-cols-1 md:grid-cols-3 gap-14 justify-items-center px-10'>
             {/* Book component */}
             <div className='bg-white w-[230px] shadow-md rounded-md flex justify-center items-start overflow-hidden'>
