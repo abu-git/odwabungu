6cb3b9db8c86ee0bb465153c18341d1ee09d89fd
Samuel Abu <abu.sammie@gmail.com>
2023-06-05 15:57:41 +0200

    books edit

--- components/Books.js
+++ components/Books.js
@@ -4,19 +4,19 @@ import Image from 'next/image'
 function Books() {
   return (
     <main className='bg-sky-50 py-10'>
-        
+        <h3 className='text-center text-3xl font-bold underline decoration-4 underline-offset-8'>Books</h3>
         <div className='py-9 grid grid-cols-1 md:grid-cols-3 gap-14 justify-items-center px-10'>
             {/* Book component */}
-            <div className='bg-white w-[230px] h-[420px] shadow-md rounded-md flex justify-center items-start overflow-hidden'>
-                <Image src="/diluted.jpg" alt="Diluted by Odwa Bungu" width={230} height={420} />
+            <div className='bg-white w-[230px] shadow-md rounded-md flex justify-center items-start overflow-hidden'>
+                <Image src="/diluted.jpg" alt="Diluted by Odwa Bungu" width={230} height={400} />
             </div>
 
-            <div className='bg-white w-[230px] h-[420px] shadow-md rounded-md flex justify-center items-start overflow-hidden'>
-                <Image src="/doubtless.png" alt="Doubtless by Odwa Bungu" width={230} height={420}/>
+            <div className='bg-white w-[230px] shadow-md rounded-md flex justify-center items-start overflow-hidden'>
+                <Image src="/doubtless.jpg" alt="Doubtless by Odwa Bungu" width={230} height={420}/>
             </div>
 
-            <div className='bg-white w-[230px] h-[420px] shadow-md rounded-md flex justify-center items-start overflow-hidden'>
-                <Image src="/knots.jpg" alt="Knots in my Wording by Odwa Bungu" width={230} height={420}/>
+            <div className='bg-white w-[230px] shadow-md rounded-md flex justify-center items-start overflow-hidden'>
+                <Image src="/knots1.jpg" alt="Knots in my Wording by Odwa Bungu" width={230} height={400}/>
             </div>
         </div>
     </main>
