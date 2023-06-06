fb41e5d8edd824fc8dc6faa5764ae5854464a711
Samuel Abu <abu.sammie@gmail.com>
2023-06-06 20:38:52 +0200

    books amazon coffee ml-7 added

--- components/Interview.js
+++ components/Interview.js
@@ -7,7 +7,7 @@ function Interview() {
         <h4 className='text-xl font-bold text-center py-4'>Keith Ngesi Interview</h4>
 
         <div className='mt-7 max-w-xl mx-auto'>
-            <iframe className="w-full aspect-video rounded-md" src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2Fkeithngesiradio%2Fvideos%2F342190614184305%2F&show_text=false&width=560&t=0" width="560" height="314" style={{ border:'none', overflow: 'hidden' }} scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
+            <iframe className="w-full aspect-video rounded-md" src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2Fkeithngesiradio%2Fvideos%2F342190614184305%2F&show_text=false&width=560&t=0" width="560" height="314" style={{ border:'none', overflow: 'hidden' }} scrolling="no" frameBorder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen={true}></iframe>
         </div>
     </div>
   )
