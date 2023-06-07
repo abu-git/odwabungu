8fb6e30d182809ad4fb96623a7861968c628566c
Samuel Abu <abu.sammie@gmail.com>
2023-06-07 12:46:42 +0200

    favicon and meta started

--- pages/index.js
+++ pages/index.js
@@ -12,12 +12,12 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Odwa Bungu | Author</title>
+        <title>Odwa Bungu | Author and Publisher</title>
         <meta name="description" content="A digital portfolio of Odwa Bungu" />
         <meta property='og:title' content="Author and Publisher" />
         <meta property='og:description' content="A digital portfolio of Odwa Bungu" />
-        <meta  property='og:image' itemProp="image" content='https://res.cloudinary.com/dbqn6vejg/image/upload/v1667741571/meta_fincj7.jpg'/>
-        <meta  property='og:image:secure_url' content='https://res.cloudinary.com/dbqn6vejg/image/upload/v1667741571/meta_fincj7.jpg'/>
+        <meta  property='og:image' itemProp="image" content='https://res.cloudinary.com/dbqn6vejg/image/upload/v1686134510/meta_kykcqm.jpg'/>
+        <meta  property='og:image:secure_url' content='https://res.cloudinary.com/dbqn6vejg/image/upload/v1686134510/meta_kykcqm.jpg'/>
         <meta property="og:url" content="https://odwabungu.vercel.app/" />
         <meta property="og:image:width" content="2024" />
         <meta property="og:image:height" content="1012" />
@@ -25,7 +25,7 @@ export default function Home() {
 
         <meta property='twitter:card' content='summary_large_image' />
         <meta property='twitter:title' content="Author and Publisher" />
-        <meta property='twitter:image' content='https://res.cloudinary.com/dbqn6vejg/image/upload/v1667741571/meta_fincj7.jpg' />
+        <meta property='twitter:image' content='https://res.cloudinary.com/dbqn6vejg/image/upload/v1686134510/meta_kykcqm.jpg' />
         <meta name="twitter:creator" content="@reachmhp"/>
         <meta name="twitter:creator" content="@reachmhp" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
