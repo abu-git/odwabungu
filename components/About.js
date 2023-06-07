8a0a964c8b998c38c1c02fd49bb8b8d35d223bfa
Samuel Abu <abu.sammie@gmail.com>
2023-06-07 15:17:09 +0200

    about edited

--- components/About.js
+++ components/About.js
@@ -27,7 +27,7 @@ function About() {
 
         <section className='px-8 md:px-24 lg:px-80 text-justify'>
             <p className='text-lg md:text-xl'>
-            Odwa Bungu is an Author / Poet/ Director of a Publishing Company and Artist who hails from Mdantsane; Eastern Cape: South Africa. He was born on the 5th of March 1992 in Frere Hospital which is one of the Greatest Hospitals in the Eastern Cape. At the age of 7 Odwa Bungu started writing short stories/ Plays and Poetry after being inspired by his grade 1 teacher Mrs Hendrix at St Johns Road Primary School in East London. Due to lack of opportunities in the Townships he couldn't publish his stories. In 2018 He got his first break as a publisher; publishing his first book titled &apos;To tame a loving Heart&apos; which was an EBook. Later that year he decided to unpublish the book and re-edit it for later use. In 2021 He published his first Poetry book titled &apos;Diluted&apos; and in 2022 He published his second Book titled Doubtless. In 2023 he recently published a book titled: &apos;Knots in my Wording&apos;. 
+            Odwa Bungu is an Author / Poet/ Director of a Publishing Company and Artist who hails from Mdantsane; Eastern Cape: South Africa. He was born on the 5th of March 1992 in Frere Hospital which is one of the Greatest Hospitals in the Eastern Cape. At the age of 7 Odwa Bungu started writing short stories/ Plays and Poetry after being inspired by his grade 1 teacher Mrs Hendrix at St Johns Road Primary School in East London. Due to lack of opportunities in the Townships he couldn&apos;t publish his stories. In 2018 He got his first break as a publisher; publishing his first book titled &apos;To tame a loving Heart&apos; which was an EBook. Later that year he decided to unpublish the book and re-edit it for later use. In 2021 He published his first Poetry book titled &apos;Diluted&apos; and in 2022 He published his second Book titled Doubtless. In 2023 he recently published a book titled: &apos;Knots in my Wording&apos;. 
             </p>
 
             <div className='lg:px-36 py-5'>
@@ -45,7 +45,7 @@ function About() {
             
 
             <p className='text-lg md:text-xl mt-10'>
-            Afroglyphics Publisher's is a publishing company that aims to publish, market and exhibit African Art with a vision of Narrating African stories through the view of African people.  It was established in 2018 on the 4th of April at Mdantsane. It publishes Paintings, Books, Music, Films and other forms of Art. Odwa Bungu is a Director at Afroglyphics Publishers.
+            Afroglyphics Publisher&apos;s is a publishing company that aims to publish, market and exhibit African Art with a vision of Narrating African stories through the view of African people.  It was established in 2018 on the 4th of April at Mdantsane. It publishes Paintings, Books, Music, Films and other forms of Art. Odwa Bungu is a Director at Afroglyphics Publishers.
             </p>
         </section>
     </div>
