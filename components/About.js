623fbf3d3d5a238d689b3d1b22453b8d63c1a125
Samuel Abu <abu.sammie@gmail.com>
2023-06-07 12:32:20 +0200

    gallery added

--- components/About.js
+++ components/About.js
@@ -1,6 +1,25 @@
 import React from 'react'
+import  ImageGallery  from 'react-image-gallery'
 
 function About() {
+  //slide array
+  const images = [
+    {
+        original: "/slider1.png",
+    },
+    {
+        original: "/slider2.png",
+    },
+    {
+        original: "/slider3.png",
+    },
+    {
+        original: "/slider4.png",
+    },
+    {
+        original: "/slider5.png",
+    }
+  ]
   return (
     <div className='bg-white py-8'>
       <div name="about"></div>
@@ -10,6 +29,20 @@ function About() {
             <p className='text-lg md:text-xl'>
             Odwa Bungu is an Author and Publisher from Mdantsane in the Eastern Cape of South Africa. A Bachelor of Arts graduate from the University of the Western Cape specializing in Language and Communications / Anthropology, he has stayed active since. He is Director of a small publishing company called Afroglyphics Publishers and has launched three books under that umbrella all available on Amazon.
             </p>
+
+            <div className='lg:px-36 py-5'>
+              <ImageGallery
+                items={images}
+                infinite={true}
+                showBullets={false}
+                showPlayButton={false}
+                showFullscreenButton={true}
+                autoPlay={true}
+                showNav={true}
+                className="w-screen" 
+              />
+            </div>
+            
 
             <p className='text-lg md:text-xl mt-10'>
             His recent book &apos;Knots in my Wording&apos; deals with the issues of stuttering and he draws inspiration from his personal experience as a young man. 
